f002aa81563583ab1e1a556a123ec3f7c19dc36a
cazcullimore <cazcullimore@gmail.com>
2025-05-10 12:29:28 -0600

    added updates for connecting and disconnecting bluetooth devices

--- HRVApp/ContentView.swift
+++ HRVApp/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreBluetooth
 
-class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralManagerDelegate, CBPeripheralDelegate {
+class BluetoothHandler: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralManagerDelegate, CBPeripheralDelegate {
     var centralManager: CBCentralManager!
     var peripheralManager: CBPeripheralManager!
     var watch: CBPeripheral!
@@ -16,7 +16,8 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, 
 
     let heartRateService = CBUUID(string: "0x180D") // heart rate serivce ID
     let deviceInfoService = CBUUID(string: "0x180A")
-    let characteristicUUID = CBUUID(string: "9C9F1559-B149-D543-26B8-D03857A26DDA")
+    
+    let heartRateMeasurmentCharacteristic = CBUUID(string: "0x2A37")
     var characteristic: CBMutableCharacteristic?
     override init() {
         super.init()
@@ -28,7 +29,7 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, 
         if central.state == .poweredOn {
             let connectedPeripherals = centralManager.retrieveConnectedPeripherals(withServices: [heartRateService])
             if !connectedPeripherals.isEmpty {
-                watch =  connectedPeripherals[0]
+                watch =  connectedPeripherals[0] // Assumes only one previously connected watch
                 centralManager.connect(watch)
                 print("conected", connectedPeripherals)
                 for per in connectedPeripherals {
@@ -51,7 +52,7 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("did connect", peripheral.name)
         peripheral.delegate = self
-        print("services", peripheral.discoverServices(nil))
+        peripheral.discoverServices([heartRateService])
     }
 
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
@@ -60,12 +61,7 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, 
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         print("peripheral updated state")
         if peripheral.state == .poweredOn {
-            let characteristic = CBMutableCharacteristic(type: characteristicUUID, properties: [.notify, .read, .write], value: nil, permissions: [.readable, .writeable])
-//            let service = CBMutableService(type: serviceUUID, primary: true)
-//            service.characteristics = [characteristic]
-//            peripheralManager.add(service)
-//            self.characteristic = characteristic
-//            peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [serviceUUID]])
+            
         } else {
             print("Peripheral is not available.")
         }
@@ -73,24 +69,13 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, 
 
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
         print("peripheral received read")
-        if request.characteristic.uuid == characteristicUUID {
-            if let value = "Hello Central".data(using: .utf8) {
-                request.value = value
-                peripheralManager.respond(to: request, withResult: .success)
-            }
-        }
+    }
+    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: (any Error)?) {
+        print("updated notification state", characteristic)
     }
 
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
         print("peripheral received write")
-        for request in requests {
-            if request.characteristic.uuid == characteristicUUID {
-                if let value = request.value, let message = String(data: value, encoding: .utf8) {
-                    print("Received message: \(message)")
-                }
-                peripheralManager.respond(to: request, withResult: .success)
-            }
-        }
     }
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         print(peripheral.name ?? "unnamed peripheral", "did discover service", peripheral.services)
@@ -100,42 +85,65 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, 
                 }
             }
         }
-    //
+    
         func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
             print("discover chars")
             if let characteristics = service.characteristics {
                 for characteristic in characteristics {
-                    print("chars", characteristic)
-                    if characteristic.uuid == characteristicUUID {
-                        peripheral.setNotifyValue(true, for: characteristic)
-                        if let message = "Hello Peripheral".data(using: .utf8) {
-                            peripheral.writeValue(message, for: characteristic, type: .withResponse)
-                        }
+                    if characteristic.properties.contains(.notify) {
+                      peripheral.setNotifyValue(true, for: characteristic)
+                        
                     }
+
+                    peripheral.readValue(for: characteristic)
+                    
+
                 }
             }
         }
 
         func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-            print("CHAR UUID", characteristic.uuid)
-            if characteristic.uuid == characteristicUUID {
-                if let value = characteristic.value, let message = String(data: value, encoding: .utf8) {
-                    print("Received message: \(message)")
-                }
+            print("updated")
+            if heartRateMeasurmentCharacteristic == characteristic.uuid {
+                print("CHAR",characteristic, characteristic.value, error)
+                let hr = heartRate(from: characteristic)
+                print("heart rate",  hr)
             }
         }
+    private func heartRate(from characteristic: CBCharacteristic) -> Int {
+      guard let characteristicData = characteristic.value else { return 0}
+      let byteArray = [UInt8](characteristicData)
+        if byteArray.count < 2 {
+            return -1
+        }
+      let firstBitValue = byteArray[0] & 0x01
+      if firstBitValue == 0 {
+        // Heart Rate Value Format is in the 2nd byte
+        return Int(byteArray[1])
+      } else {
+        // Heart Rate Value Format is in the 2nd and 3rd bytes
+        return (Int(byteArray[1]) << 8) + Int(byteArray[2])
+      }
+    }
+    func connectButtonPressed(peripheralName: String){
+        if peripheralNameToObj[peripheralName]!.state == CBPeripheralState.connected {
+            centralManager.cancelPeripheralConnection(peripheralNameToObj[peripheralName]!)
+            print("disconnect")
+            
+        }else{
+            
+            centralManager.connect(peripheralNameToObj[peripheralName]!)
+            print("connected")
+        }
+        print(peripheralNameToObj[peripheralName]!)
+        objectWillChange.send()
+    }
 }
-
-// extension BluetoothViewModel: CBPeripheralDelegate {
-//    
-//
-//    
-// }
 
 struct ContentView: View {
     @State private var doHRV = false
     @State private var hrSampleRate = 5
-    @ObservedObject private var bluetooth = BluetoothViewModel()
+    @ObservedObject private var bluetooth = BluetoothHandler()
     var body: some View {
         NavigationView {
             VStack {
@@ -188,18 +196,17 @@ struct ContentView: View {
 
                 NavigationLink(destination:
                                 VStack {
-                    Text("Sample Heart Rate Every " + String(hrSampleRate) + " mins")
-                    Picker("Heart Rate Sample Rate ", selection: $hrSampleRate) {
-                        ForEach(0..<60) { Text("\($0) mins") }
+                    Text("Sample heart rate every " + ((hrSampleRate == 1) ? "minute" : "\(hrSampleRate) minutes"))
+                    Picker("Heart Rate Sample Rate", selection: $hrSampleRate) {
+                        ForEach(0..<60) { Text(($0 == 1) ? "\($0) minute" : "\($0) minutes") }
                     }
-                    .frame(width: 100)
-                    //                                            .clipped()
+                    .frame(width: 200)
                     .pickerStyle(.wheel)
                 }
 
                 ) {
                     Label {
-                        Text("Sample HeartRate")
+                        Text("Heart Rate Sampling Interval")
                             .font(.title3)
                             .frame(maxWidth: .infinity, alignment: .leading)
                     } icon: {}
@@ -209,37 +216,49 @@ struct ContentView: View {
 
                     ) {
                         Label {
-                            Text("Go To Graphs")
+                            Text("Graphs")
                                 .font(.title3)
                                 .frame(maxWidth: .infinity, alignment: .leading)
 
-                        } icon: {
-                            Image(systemName: "chart.line.text.clipboard")
-                        }
+                        } icon: {}
 
                     }
                     .navigationTitle("Summary")
 
                 NavigationLink(destination:
                     List(Array(bluetooth.peripheralNameToObj.keys), id: \.self) {
-                        peripheralName in Button(peripheralName, action: {bluetooth.centralManager.connect(bluetooth.peripheralNameToObj[peripheralName]!)
+                    peripheralName in Button(getDeviceButtonName(peripheralName: peripheralName), action: {bluetooth.connectButtonPressed(peripheralName: peripheralName)
                         })
                     }
                     .navigationTitle("Peripherals")
 
                     ) {
-                        Label("connect to device", systemImage: "dot.radiowaves.left.and.right")
+                    Label{Text("Device")} icon:{}
+                        .font(.title3)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
             }
+            
         }
         .padding()
     }
+    func getDeviceButtonName(peripheralName:String) -> String {
+        var name = peripheralName + " "
+        switch bluetooth.peripheralNameToObj[peripheralName]!.state {
+        case CBPeripheralState.connected:
+            name += "(Connected)"
+        case CBPeripheralState.connecting:
+            name += "(Connecting)"
+        default:
+            name += "(Disconnected)"
+        }
+        
+        return name
+        
+    }
+
 }
 
 #Preview {
     ContentView()
-}
-
-func run(param: CBPeripheral) {
-    print("ran", param)
 }
