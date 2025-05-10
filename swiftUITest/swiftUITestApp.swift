a5cc906c5fda8eac7f4b3066db567b7441d4013c
cazcullimore <cazcullimore@gmail.com>
2025-05-09 21:48:35 -0600

    Initial Commit

---
+++ swiftUITest/swiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITestApp.swift
+//  swiftUITest
+//
+//  Created by Caz Cullimore on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUITestTests/swiftUITestTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITestTests.swift
+//  swiftUITestTests
+//
+//  Created by Caz Cullimore on 5/9/25.
+//
+
+import Testing
+@testable import swiftUITest
+
+struct swiftUITestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
